566ed4d78a0ef5ca9fc7f717a5735c136786250d
Sonu208 <sonupradhan208@gmail.com>
2024-04-09 16:05:08 +0530

    added patient list api in doctor dashboard

--- src/components/PatientRegistration.js
+++ src/components/PatientRegistration.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
-import PatientRegistry from "../build/contracts/PatientRegistry.json";
+import PatientRegistryABI from "../build/contracts/PatientRegistry.json";
 import { useNavigate } from "react-router-dom";
 import "../CSS/PatientRegistration.css";
 import NavBar from "./NavBar";
@@ -30,9 +30,9 @@ const PatientRegistration = () => {
           setWeb3(web3Instance);
 
           const networkId = await web3Instance.eth.net.getId();
-          const deployedNetwork = PatientRegistry.networks[networkId];
+          const deployedNetwork = PatientRegistryABI.networks[networkId];
           const contractInstance = new web3Instance.eth.Contract(
-            PatientRegistry.abi,
+            PatientRegistryABI.abi,
             deployedNetwork && deployedNetwork.address
           );
 
@@ -50,57 +50,38 @@ const PatientRegistration = () => {
 
   const handleRegister = async () => {
     try {
-      if (window.ethereum) {
-        const web3Instance = new Web3(window.ethereum);
-        try {
-          if (isRegistered) {
-            alert("User is already registered.");
-            return;
-          }
-          await window.ethereum.enable();
-          setWeb3(web3Instance);
+      if (!contract) {
+        console.error("Contract not initialized.");
+        return;
+      }
 
-          const networkId = await web3Instance.eth.net.getId();
-          const deployedNetwork = PatientRegistry.networks[networkId];
-          const contractInstance = new web3Instance.eth.Contract(
-            PatientRegistry.abi,
-            deployedNetwork && deployedNetwork.address
-          );
-
-          setContract(contractInstance);
-        } catch (error) {
-          console.error("User denied access to accounts.");
-        }
-      } else {
-        console.log("Please install MetaMask extension");
+      if (isRegistered) {
+        alert("User is already registered.");
+        return;
       }
 
       if (
         !name ||
         !dateOfBirth ||
-        !dateOfBirth ||
-        !gender ||
-        !phoneNumber ||
         !homeAddress ||
-        !walletAddress
+        !phoneNumber ||
+        !walletAddress ||
+        !gender ||
+        !password
       ) {
-        alert(
-          "You have missing input fields. Please fill in all the required fields."
-        );
+        alert("Please fill in all the required fields.");
         return;
       }
 
       if (phoneNumber.length !== 10) {
-        alert(
-          "You have entered a wrong phone number. Please enter a 10-digit phone number."
-        );
+        alert("Please enter a 10-digit phone number.");
         return;
       }
 
-      // Check if dateOfBirth is in the format dd/mm/yyyy
+      // Check if dateOfBirth is in the format yyyy-mm-dd
       const datePattern = /^\d{4}-\d{2}-\d{2}$/;
       if (!datePattern.test(dateOfBirth)) {
-        alert("Please enter Date of Birth in the format dd/mm/yyyy");
+        alert("Please enter Date of Birth in the format yyyy-mm-dd");
         return;
       }
 
@@ -111,9 +92,9 @@ const PatientRegistration = () => {
       }
 
       setIsLoading(true);
-      const isRegPatient = await contract.methods
-        .isRegisteredPatient(walletAddress)
-        .call({ from: walletAddress });
+
+      const isRegPatient = await contract.methods.isRegisteredPatient(walletAddress).call();
+
 
       if (isRegPatient) {
         setIsRegistered(true);
@@ -133,20 +114,13 @@ const PatientRegistration = () => {
           password
         )
         .send({ from: walletAddress });
-      setName("");
-      setHomeAddress("");
-      setDateOfBirth("");
-      setPhoneNumber("");
-      setWalletAddress("");
-      setGender("");
-      setPassword("");
 
-      alert("Patient registered successfully!");
-      navigate("/patient_login");
-    } catch (error) {
-      console.error("Error registering patient:", error);
-    } finally {
-      setIsLoading(false);
+        setIsLoading(false);
+        alert("Patient registered successfully!");
+        navigate("/patient_login");
+      } catch (error) {
+        console.error("Error registering patient:", error);
+        setIsLoading(false);
     }
   };
 
@@ -164,147 +138,159 @@ const PatientRegistration = () => {
 
   return (
     <div>
-    <NavBar></NavBar>
-    <div className="min-h-screen flex items-center justify-center p-4 bg-gradient-to-b from-black to-gray-800 font-mono">
-      <div className="w-full max-w-4xl">
-        <h2 className="text-3xl text-white mb-6 font-bold text-center">
-          Register Patient
-        </h2>
-        <form className="bg-gray-900 p-6 rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="mb-4">
-            <label className="block font-bold text-white" htmlFor="name">
-              Patient Full Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Patient's Full Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
-            />
-          </div>
+      <NavBar />
+      <div className="min-h-screen flex items-center justify-center p-4 bg-gradient-to-b from-black to-gray-800 font-mono">
+        <div className="w-full max-w-4xl">
+          <h2 className="text-3xl text-white mb-6 font-bold text-center">
+            Register Patient
+          </h2>
+          <form className="bg-gray-900 p-6 rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="mb-4">
+              <label className="block font-bold text-white" htmlFor="name">
+                Patient Full Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Patient's Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block font-bold text-white" htmlFor="dateOfBirth">
-              Date of Birth:
-            </label>
-            <input
-              id="dateOfBirth"
-              name="dateOfBirth"
-              type="date" // Use type="date" for date picker
-              required
-              className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover-bg-gray-800 transition duration-200"
-              value={dateOfBirth}
-              onChange={(e) => setDateOfBirth(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                className="block font-bold text-white"
+                htmlFor="dateOfBirth"
+              >
+                Date of Birth:
+              </label>
+              <input
+                id="dateOfBirth"
+                name="dateOfBirth"
+                type="date"
+                required
+                className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover-bg-gray-800 transition duration-200"
+                value={dateOfBirth}
+                onChange={(e) => setDateOfBirth(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block font-bold text-white" htmlFor="homeAddress">
-              Home Address:
-            </label>
-            <input
-              type="text"
-              id="homeAddress"
-              name="homeAddress"
-              placeholder="Permanent Address"
-              value={homeAddress}
-              onChange={(e) => setHomeAddress(e.target.value)}
-              className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                className="block font-bold text-white"
+                htmlFor="homeAddress"
+              >
+                Home Address:
+              </label>
+              <input
+                type="text"
+                id="homeAddress"
+                name="homeAddress"
+                placeholder="Permanent Address"
+                value={homeAddress}
+                onChange={(e) => setHomeAddress(e.target.value)}
+                className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block font-bold text-white" htmlFor="phoneNumber">
-              Phone Number:
-            </label>
-            <input
-              id="phoneNumber"
-              name="phoneNumber"
-              type="text"
-              required
-              className={`mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover-bg-gray-800 transition duration-200 ${phoneNumberError && "border-red-500"}`}
-              placeholder="Phone Number"
-              value={phoneNumber}
-              onChange={handlePhoneNumberChange}
-            />
-            {phoneNumberError && (
-              <p className="text-red-500 text-sm mt-1">{phoneNumberError}</p>
-            )}
-          </div>
+            <div className="mb-4">
+              <label
+                className="block font-bold text-white"
+                htmlFor="phoneNumber"
+              >
+                Phone Number:
+              </label>
+              <input
+                id="phoneNumber"
+                name="phoneNumber"
+                type="text"
+                required
+                className={`mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover-bg-gray-800 transition duration-200 ${phoneNumberError && "border-red-500"
+                  }`}
+                placeholder="Phone Number"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+              />
+              {phoneNumberError && (
+                <p className="text-red-500 text-sm mt-1">{phoneNumberError}</p>
+              )}
+            </div>
 
-          <div className="mb-4">
-            <label
-              className="block font-bold text-white"
-              htmlFor="walletAddress"
-            >
-              Patient's Wallet Public Address:
-            </label>
-            <input
-              type="text"
-              id="walletAddress"
-              name="walletAddress"
-              placeholder="Public Address"
-              value={walletAddress}
-              onChange={(e) => setWalletAddress(e.target.value)}
-              className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                className="block font-bold text-white"
+                htmlFor="walletAddress"
+              >
+                Patient's Wallet Public Address:
+              </label>
+              <input
+                type="text"
+                id="walletAddress"
+                name="walletAddress"
+                placeholder="Public Address"
+                value={walletAddress}
+                onChange={(e) => setWalletAddress(e.target.value)}
+                className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block font-bold text-white" htmlFor="gender">
-              Gender:
-            </label>
-            <select
-              id="gender"
-              name="gender"
-              required
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-              className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover-bg-gray-800 transition duration-200"
-            >
-              <option value="">Select Gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
+            <div className="mb-4">
+              <label
+                className="block font-bold text-white"
+                htmlFor="gender"
+              >
+                Gender:
+              </label>
+              <select
+                id="gender"
+                name="gender"
+                required
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover-bg-gray-800 transition duration-200"
+              >
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
 
-          <div className="mb-4">
-            <label
-              className="block font-bold text-white"
-              htmlFor="password"
-            >
-              Password:
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              required
-              className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover-bg-gray-800 transition duration-200"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                className="block font-bold text-white"
+                htmlFor="password"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="mt-2 p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
+              />
+            </div>
 
-          <div className="md:col-span-2 flex justify-center mt-6">
-            <button
-              type="button"
-              onClick={handleRegister}
-              disabled={!contract || isLoading}
-              className="px-5 py-2.5 bg-teal-500 text-white font-bold text-lg rounded-lg cursor-pointer transition-colors duration-300 ease-in-out hover:bg-gray-600 disabled:bg-gray-500 disabled:cursor-not-allowed w-full md:w-auto"
-            >
-              Register
-            </button>
-          </div>
-        </form>
+            <div className="mb-4">
+              <button
+                type="button"
+                onClick={handleRegister}
+                disabled={isLoading}
+                className="w-full px-4 py-2 bg-teal-500 hover:bg-teal-600 text-white font-bold rounded-md transition duration-200"
+              >
+                {isLoading ? "Registering..." : "Register"}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
--- src/components/RecordPermission.js
+++ src/components/RecordPermission.js
@@ -1,21 +1,24 @@
+// RecordPermission.js
+
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
-import record from "../build/contracts/record.json"; // Replace with the correct path to your ABI JSON
-import DoctorRegistration from "../build/contracts/DoctorRegistration.json";
+import recordABI from "../build/contracts/record.json";
 import "../CSS/RecordPermission.css";
 import { useNavigate, useParams } from "react-router-dom";
 import NavBar_Logout from "./NavBar_Logout";
+import PatientRegistryABI from "../build/contracts/PatientRegistry.json";
 
 function RecordPermission() {
   const navigate = useNavigate();
   const { address } = useParams();
   const [web3, setWeb3] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [contractInstance2, setContractInstance2] = useState(null);
+  const [recordContractInstance, setRecordContractInstance] = useState(null);
+  const [patientRegistryContractInstance, setPatientRegistryContractInstance] = useState(null);
   const [patientAddress, setPatientAddress] = useState("");
   const [doctorAddress, setDoctorAddress] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [permissionGranted, setPermissionGranted] = useState(false);
+  const [patientName, setPatientName] = useState(""); // Add state for patient name
 
   useEffect(() => {
     const init = async () => {
@@ -26,20 +29,36 @@ function RecordPermission() {
           setWeb3(web3Instance);
 
           const networkId = await web3Instance.eth.net.getId();
-          const deployedNetwork = record.networks[networkId];
-          const deployedNetwork2 = DoctorRegistration.networks[networkId];
-          const contractInstance = new web3Instance.eth.Contract(
-            record.abi,
-            deployedNetwork && deployedNetwork.address
+          const deployedRecordNetwork = recordABI.networks[networkId];
+          const deployedPatientRegistryNetwork = PatientRegistryABI.networks[networkId]; // Use patient registry ABI
+          
+          const recordContract = new web3Instance.eth.Contract(
+            recordABI.abi,
+            deployedRecordNetwork && deployedRecordNetwork.address
           );
 
-          const contractInstance2 = new web3Instance.eth.Contract(
-            DoctorRegistration.abi,
-            deployedNetwork2 && deployedNetwork2.address
+          const patientRegistryContract = new web3Instance.eth.Contract(
+            PatientRegistryABI.abi,
+            deployedPatientRegistryNetwork && deployedPatientRegistryNetwork.address
           );
 
-          setContract(contractInstance);
-          setContractInstance2(contractInstance2);
+          setRecordContractInstance(recordContract);
+          setPatientRegistryContractInstance(patientRegistryContract);
+
+          // Listen for the event emitted when a new patient is registered
+          patientRegistryContractInstance.events.PatientRegistered({ fromBlock: 'latest' })
+            .on('data', async (event) => {
+              // Retrieve the patient's name from the event data
+              const { patientAddress: registeredAddress, name } = event.returnValues;
+
+              // Update the state with the patient's name
+              if (registeredAddress.toLowerCase() === patientAddress.toLowerCase()) {
+                setPatientName(name);
+              }
+            })
+            .on('error', (error) => {
+              console.error("Error listening for PatientRegistered event:", error);
+            });
         } catch (error) {
           console.error("User denied access to accounts.");
         }
@@ -49,12 +68,12 @@ function RecordPermission() {
     };
 
     init();
-  }, []);
+  }, [patientAddress, patientRegistryContractInstance]); // Add patientAddress and patientRegistryContractInstance to the dependency array
 
   const grantPermission = async () => {
     try {
       setIsLoading(true);
-      await contract.methods
+      await recordContractInstance.methods
         .grantPermission(doctorAddress)
         .send({ from: patientAddress });
       setPermissionGranted(true);
@@ -68,7 +87,7 @@ function RecordPermission() {
   const revokePermission = async () => {
     try {
       setIsLoading(true);
-      await contract.methods
+      await recordContractInstance.methods
         .revokePermission(doctorAddress)
         .send({ from: patientAddress });
       setPermissionGranted(false);
@@ -81,7 +100,7 @@ function RecordPermission() {
 
   const cancelOperation = async () => {
     try {
-    navigate("/patient/"+address);
+      navigate("/patient/" + address);
     } catch (error) {
       console.error("Error checking permission:", error);
     }
@@ -89,54 +108,58 @@ function RecordPermission() {
 
   return (
     <div>
-    <NavBar_Logout></NavBar_Logout>
-    <div className="bg-gradient-to-b from-black to-gray-800 min-h-screen flex items-center justify-center p-4 font-mono text-white">
-      <div className="w-full max-w-6xl bg-gray-900 p-20 rounded-lg shadow-lg">
-        <h1 className="text-3xl mb-6 font-bold text-center">
-          Record Permission
-        </h1>
-        <div className="flex flex-col w-full mb-4">
-          <label className="mb-2 font-bold">Patient Address:</label>
-          <input
-            type="text"
-            value={patientAddress}
-            onChange={(e) => setPatientAddress(e.target.value)}
-            className="p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
-          />
-        </div>
-        <div className="flex flex-col w-full mb-4">
-          <label className="mb-2 font-bold">Doctor Address:</label>
-          <input
-            type="text"
-            value={doctorAddress}
-            onChange={(e) => setDoctorAddress(e.target.value)}
-            className="p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
-          />
-        </div>
-        <div className="flex gap-4">
-          <button
-            onClick={grantPermission}
-            disabled={!contract || isLoading}
-            className="px-5 py-2.5 bg-green-500 text-white font-bold text-lg rounded-lg cursor-pointer transition-transform transition-colors duration-300 ease-in hover:bg-teal-600 active:bg-teal-700"
-          >
-            Grant Permission
-          </button>
-          <button
-            onClick={revokePermission}
-            className="px-5 py-2.5 bg-red-500 text-white font-bold text-lg rounded-lg cursor-pointer transition-transform transition-colors duration-300 ease-in hover:bg-teal-600 active:bg-teal-700"
-          >
-            Revoke Permission
-          </button>
-          <button
-          onClick={cancelOperation}
-          className="px-6 py-3 bg-teal-500 text-white font-bold text-lg rounded-lg cursor-pointer transition-transform transition-colors duration-300 ease-in hover:bg-teal-600 active:bg-teal-700"
-        >
-          Cancel
-          </button>
+      <NavBar_Logout />
+      <div className="bg-gradient-to-b from-black to-gray-800 min-h-screen flex items-center justify-center p-4 font-mono text-white">
+        <div className="w-full max-w-6xl bg-gray-900 p-20 rounded-lg shadow-lg">
+          <h1 className="text-3xl mb-6 font-bold text-center">Record Permission</h1>
+          <div className="flex flex-col w-full mb-4">
+            <label className="mb-2 font-bold">Patient Address:</label>
+            <input
+              type="text"
+              value={patientAddress}
+              onChange={(e) => setPatientAddress(e.target.value)}
+              className="p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
+            />
+            <button
+              className="px-4 py-2 mt-2 bg-teal-500 hover:bg-teal-600 text-white rounded-md transition duration-200"
+            >
+              Fetch Name
+            </button>
+            <p className="mt-2 text-gray-400">{patientName}</p>
+          </div>
+          <div className="flex flex-col w-full mb-4">
+            <label className="mb-2 font-bold">Doctor Address:</label>
+            <input
+              type="text"
+              value={doctorAddress}
+              onChange={(e) => setDoctorAddress(e.target.value)}
+              className="p-2 w-full text-white bg-gray-700 border border-gray-600 rounded-md hover:bg-gray-800 transition duration-200"
+            />
+          </div>
+          <div className="flex gap-4">
+            <button
+              onClick={grantPermission}
+              disabled={!recordContractInstance || isLoading}
+              className="px-5 py-2.5 bg-green-500 text-white font-bold text-lg rounded-lg cursor-pointer transition-transform transition-colors duration-300 ease-in hover:bg-teal-600 active:bg-teal-700"
+            >
+              Grant Permission
+            </button>
+            <button
+              onClick={revokePermission}
+              className="px-5 py-2.5 bg-red-500 text-white font-bold text-lg rounded-lg cursor-pointer transition-transform transition-colors duration-300 ease-in hover:bg-teal-600 active:bg-teal-700"
+            >
+              Revoke Permission
+            </button>
+            <button
+              onClick={cancelOperation}
+              className="px-6 py-3 bg-teal-500 text-white font-bold text-lg rounded-lg cursor-pointer transition-transform transition-colors duration-300 ease-in hover:bg-teal-600 active:bg-teal-700"
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 }
 
